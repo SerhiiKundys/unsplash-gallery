2c5859afe1854e2405e39206737bed0dd4a4d9a5
Serhii <sergiykundys@gmail.com>
2020-07-05 21:54:09 +0300

    added screens

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = "YOUR-TOKEN-b123cb312ced1341f13ed213c ";
+const CLIENT_ID = "YOUR-TOKEN-b123cb312ced1341f13ed213c";
 
 export const fetchNewData = async (newPage) => {
   const url = `https://api.unsplash.com/photos/?page=${newPage}&client_id=${CLIENT_ID}`;
